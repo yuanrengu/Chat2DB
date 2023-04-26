b3d140b4a75dea3f13443d5ddb145a12dbd65625
fanjin.fjy <fanjin.fjy@alibaba-inc.com>
2023-04-26 13:51:34 +0800

    feat: 调整link

--- src/components/homepage/APIReferenceSection.jsx
+++ src/components/homepage/APIReferenceSection.jsx
@@ -15,15 +15,15 @@ export default function APIReferenceSection() {
         <Link
           href="/guides/quickstart"
           aria-label="AI Chat SQL"
-          target="_blank"
-          className="absolute top-8 right-8 flex h-16 w-16 items-center justify-center rounded-full bg-zinc-600/40 dark:bg-transparent"
+          className="absolute right-8 top-8 flex h-16 w-16 items-center justify-center rounded-full bg-zinc-600/40 dark:bg-transparent"
         >
           <ArrowUpRight className="h-6 w-6 text-zinc-400 dark:text-black" />
         </Link>
         <div className="flex-1">
           <h2 className="text-4xl">AI Chat SQL</h2>
           <p className="text-zinc-400">
-            Chat2DB包含一系列基于ChatGPT的AI SQL使用功能，主要包括自然语言转SQL、SQL解释、SQL优化和SQL转换。
+            Chat2DB包含一系列基于ChatGPT的AI
+            SQL使用功能，主要包括自然语言转SQL、SQL解释、SQL优化和SQL转换。
             使用这些AI功能，可以将自然语言转换成本地查询SQL，而不仅仅是SQL查询伪代码；可以将SQL解释成自然语言，帮助用户理解复杂的SQL；
             可以针对慢SQL提供全方位的优化建议，提升查询效率；可以转换不同数据库类型的SQL语言，降低数据库迁移难度等等。
           </p>
@@ -44,9 +44,7 @@ export default function APIReferenceSection() {
                   &rarr;
                 </span>
               </Link>
-              <div className="text-zinc-400">
-                输入自然语言，输出查询SQL
-              </div>
+              <div className="text-zinc-400">输入自然语言，输出查询SQL</div>
             </li>
             <li className="flex flex-col gap-1">
               <Link
@@ -58,9 +56,7 @@ export default function APIReferenceSection() {
                   &rarr;
                 </span>
               </Link>
-              <div className="text-zinc-400">
-                输入SQL，输出自然语言
-              </div>
+              <div className="text-zinc-400">输入SQL，输出自然语言</div>
             </li>
             <li className="flex flex-col gap-1">
               <Link
@@ -78,8 +74,8 @@ export default function APIReferenceSection() {
             </li>
             <li className="flex flex-col gap-1">
               <Link
-                  href="https://github.com/alibaba/Chat2DB/blob/main/CHAT2DB_AI_SQL.md#sql%E4%BC%98%E5%8C%96"
-                  className="group font-jakarta font-semibold text-current"
+                href="https://github.com/alibaba/Chat2DB/blob/main/CHAT2DB_AI_SQL.md#sql%E4%BC%98%E5%8C%96"
+                className="group font-jakarta font-semibold text-current"
               >
                 SQL转换
                 <span className="ml-2 opacity-0 transition group-hover:translate-x-2 group-hover:opacity-100">

85d32fff1aeb18e54f939f77fa8b0420b436f27e
Jiaju Zhuang <zhuangjiaju@qq.com>
2023-04-28 11:35:59 +0800

    删除搜索

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -441,14 +441,6 @@ const config = {
           },
         ],
       },
-      // 搜索相关
-      algolia: {
-        appId: 'HL0HSV62RK',
-        apiKey: '72ebf02146698733b7114c7b36da0945',
-        indexName: 'docs',
-        contextualSearch: true,
-        searchParameters: {},
-      },
     }),
 
   webpack: {
